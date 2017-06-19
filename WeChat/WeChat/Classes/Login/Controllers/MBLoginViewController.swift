9a7a9e253f4dff7aac1bfbb4e1ba324e88138035
Mybar835 <lijingui2010@163.com>
2017-06-19 15:55:55 +0800

    搭建登陆界面
    1. 搭建登陆界面
    2. 添加AppIcon 和 LaunchImage

---
+++ WeChat/WeChat/Classes/Login/Controllers/MBLoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MBLoginViewController.swift
+//  WeChat
+//
+//  Created by lijingui2010 on 2017/6/19.
+//  Copyright © 2017年 MyBar. All rights reserved.
+//
+
+import UIKit
+
+class MBLoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginAction(_ sender: Any) {
+        
+    }
+    
+    
+
+    
+}
